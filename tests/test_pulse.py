a5fc458a1d39dda305ab707132bae38daeab7037
Richard Cook <rcook@rcook.org>
2026-01-03 22:53:21 -0800

    Test for Pulse class

---
+++ tests/test_pulse.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2026 Richard Cook
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from beat_studio_importer.misc import Bpm, MidiTempo
+from beat_studio_importer.pulse import Pulse
+import pytest
+
+
+class TestPulse:
+    @pytest.mark.parametrize("tempo, pulse, expected", [
+        (600_000, Pulse.QUARTER, 100),
+        (400_000, Pulse.QUARTER, 150),
+        (400_000, Pulse.DOTTED_QUARTER, 100),
+    ])
+    def test_basics(self, tempo: MidiTempo, pulse: Pulse, expected: Bpm) -> None:
+        assert pulse.midi_tempo_to_bpm(tempo) == expected
