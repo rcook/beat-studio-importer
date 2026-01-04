3575882006668889d04cdccfd23007ea729f8af4
Richard Cook <rcook@rcook.org>
2026-01-03 17:16:43 -0800

    Output friendly message if MIDI port is in use

--- beat_studio_importer/play_command.py
+++ beat_studio_importer/play_command.py
@@ -25,21 +25,45 @@
 # pyright: reportUnknownMemberType=false
 # pyright: reportUnknownVariableType=false
 
-from beat_studio_importer.ui import print_key_value
+from beat_studio_importer.ui import cprint, print_key_value
 from beat_studio_importer.user_error import UserError
+from collections.abc import Generator
+from colorama import Fore
+from contextlib import ExitStack, contextmanager
 from mido import MidiFile
+from mido.ports import BaseOutput
 from pathlib import Path
 import mido
 
 
-def do_play(path: Path, port_name: str | None = None, force_channel_10: bool = True) -> None:
-    if port_name is not None:
-        if port_name not in mido.get_output_names():
-            raise UserError(f"Unknown MIDI port {port_name}")
+@contextmanager
+def open_midi_port(name: str | None) -> Generator[BaseOutput, None, None]:
+    if name is not None:
+        if name not in mido.get_output_names():
+            raise UserError(f"Unknown MIDI port {name}")
 
-    with mido.open_output(port_name) as port:
+    with ExitStack() as stack:
+        try:
+            port = stack.enter_context(mido.open_output(name))
+        except Exception as e:
+            if type(e).__name__ != "SystemError":
+                raise
+            if name is None:
+                raise UserError(
+                    f"Could not open default MIDI output port: perhaps it is already in use?")
+            else:
+                raise UserError(
+                    f"Could not open MIDI output port {name}: perhaps it is already in use?")
+        yield port
+
+
+def do_play(path: Path, port_name: str | None = None, force_channel_10: bool = True) -> None:
+    with open_midi_port(port_name) as port:
         print_key_value("File", path)
         print_key_value("MIDI port", port.name)
+
+        if force_channel_10:
+            cprint(Fore.LIGHTBLUE_EX, "Will remap all notes to MIDI channel 10")
 
         file = MidiFile(path)
         for message in file.play():
